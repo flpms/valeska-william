0b49d110de0184e35a68d17b92556bbdb12a4c0c
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-03-14 18:07:18 -0300

    Criação de template e bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Valeska & William' });
 });
 
 router.get('/lista', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('lista', { title: 'Valeska & William - Lista' });
 });
 
 router.get('/cerimonia', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('cerimonia',  { title: 'Valeska & William - Cerimônia' });
 });
 
 router.get('/mensagem', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'mensagem' });
 });
 
 router.get('/confirmacao', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'confirmacao' });
 });
 
 module.exports = router;
