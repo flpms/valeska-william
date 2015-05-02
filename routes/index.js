22dd58890e0005f4396a89a3922a4330fb7f4b19
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-05-02 19:26:05 -0300

    Ajustes para e-mail com xoauth2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var xoauth2 = require('xoauth2');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,12 +23,23 @@ router.get('/mensagem', function(req, res, next) {
 router.get('/confirmacao', function(req, res, next) {
     var result;
 
+    generator = xoauth2.createXOAuth2Generator({
+        user : "filipemelodasilva@gmail.com",
+        clientSecret : "Oh1hiIjclgZg2Ew8utttckIU",
+        clientId: "272997199071-sueg5qpj4i1v0mlep1br6usio5qs335j.apps.googleusercontent.com",
+        refreshToken : "1/itBk6FJoX_YTH-QRP8pocR6nEchGS3pAqz7wEphe86s",
+        accessToken : "ya29.ZwFCtzF3Ng92868NbE51QAa8mheek_SE9fXS0qlxfiHgatGTuxSajSwYETng0IsFL58yeQyg8dF0LQ"
+    });
+
+    generator.on('token', function(token){
+        console.log('New token for %s: %s', token.user, token.accessToken);
+    });
+
     // create reusable transporter object using SMTP transport
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'filipemelodasilva@gmail.com',
-            pass: '#keila00'
+            xoauth2: generator
         }
     });
 
@@ -37,9 +49,9 @@ router.get('/confirmacao', function(req, res, next) {
     // setup e-mail data with unicode symbols
     var mailOptions = {
         from: 'filipemelodasilva@gmail.com', // sender address
-        to: 'flpms@icloud.com', // list of receivers
-        subject: 'Hello Teste NodeMailer✔', // Subject line
-        html: '<b>Hello world ✔ <br /> Se funcionar funcionou.</b>' // html body
+        to: 'keila.avilaalmeida@gmail.com', // list of receivers
+        subject: '♥ ♥ ♥ ♥ ♥ ♥ Te amo, mesmo programando ♥ ♥ ♥ ♥ ♥ ♥', // Subject line
+        html: '<b>Hello world ✔ <br /> Para dizer que está funcionando e estou acabando. < br/> ♥ ♥ ♥ ♥ ♥ ♥ ♥</b>' // html body
     };
 
     // send mail with defined transport object
@@ -57,3 +69,4 @@ router.get('/confirmacao', function(req, res, next) {
 });
 
 module.exports = router;
+
