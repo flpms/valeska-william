7a1d8437a156386b05cf2abe023bb45013a18a0c
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-05-07 14:39:33 -0300

    Construção de formulário de e-mail e mudança para get

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mail = require('./modules/Mail.js');
+var querystring = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,12 +21,28 @@ router.get('/mensagem', function(req, res, next) {
 });
 
 router.get('/confirmacao', function(req, res, next) {
-    var result = req;
-    console.log(req);
-    var dataRes = {'title': 'confirmacao'};
-    res.type('application/json');
-    res.status(200)
-    res.send(dataRes);
+    var emailContent = mail.fillContent(req.query.names,req.query.email,req.query.message);
+
+    var afterSendEmail = function(error, info) {
+        var result;
+        var dataRes = {
+        	status:''
+        };
+        if(error){
+            dataRes.status = fail;
+            dataRes.details = error;
+            console.log('error', error);
+        }else{
+            console.log('Message sent: ' + info.response);
+            result = info.response;
+            dataRes.status = 'OK';
+		    res.set({'Content-Type': 'application/json'});
+		    res.status(200).json(dataRes);
+        }
+    }
+
+    var resultSend = mail.sendEmail(emailContent, afterSendEmail);
+    
 });
 
 module.exports = router;
--- routes/modules/Mail.js
+++ routes/modules/Mail.js
@@ -1,50 +1,49 @@
 var nodemailer = require('nodemailer');
 var xoauth2 = require('xoauth2');
 
-var Mail = function(){
-	var result;
+var Mail = function() {
+    var self = this;
+    self.fillContent = function(name, from, message) {
+        var hasMessage = '';
 
-    generator = xoauth2.createXOAuth2Generator({
-        user : "filipemelodasilva@gmail.com",
-        clientSecret : "Oh1hiIjclgZg2Ew8utttckIU",
-        clientId: "272997199071-sueg5qpj4i1v0mlep1br6usio5qs335j.apps.googleusercontent.com",
-        refreshToken : "1/itBk6FJoX_YTH-QRP8pocR6nEchGS3pAqz7wEphe86s",
-        accessToken : "ya29.ZwFCtzF3Ng92868NbE51QAa8mheek_SE9fXS0qlxfiHgatGTuxSajSwYETng0IsFL58yeQyg8dF0LQ"
-    });
-
-    generator.on('token', function(token){
-        console.log('New token for %s: %s', token.user, token.accessToken);
-    });
-
-    // create reusable transporter object using SMTP transport
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            xoauth2: generator
+        if (message) {
+            hasMessage = ' e lhe enviou a mensagem a baixo';
+            message = '<p style="font-style:italic;">“'+ message +'”</p>';
+        } else {
+            message = '';
         }
-    });
 
-    // NB! No need to recreate the transporter object. You can use
-    // the same transporter object for all e-mails
+        return {
+            from: from, // sender address
+            to: 'flpms@icloud.com', // list of receivers
+            subject: '[✓] Confirmando a presença de ' + name, // Subject line
+            html: '<h1>' +name + '</h1><h3>Estará no seu casamento'+hasMessage+'</h3>'+message+'<br /><br /><br />'+email // html body
+        };
+    }
 
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-        from: 'filipemelodasilva@gmail.com', // sender address
-        to: 'flpms@icloud.com', // list of receivers
-        subject: '', // Subject line
-        html: '' // html body
-    };
+    self.sendEmail = function(content, callback) {
+        var generator = xoauth2.createXOAuth2Generator({
+            user : "filipemelodasilva@gmail.com",
+            clientSecret : "Oh1hiIjclgZg2Ew8utttckIU",
+            clientId: "272997199071-sueg5qpj4i1v0mlep1br6usio5qs335j.apps.googleusercontent.com",
+            refreshToken : "1/itBk6FJoX_YTH-QRP8pocR6nEchGS3pAqz7wEphe86s",
+            accessToken : "ya29.ZwFCtzF3Ng92868NbE51QAa8mheek_SE9fXS0qlxfiHgatGTuxSajSwYETng0IsFL58yeQyg8dF0LQ"
+        });
 
+        generator.on('token', function(token){
+            console.log('New token for %s: %s', token.user, token.accessToken);
+        });
+
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                xoauth2: generator
+            }
+        });
+
+        transporter.sendMail(content, callback);
+    }
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            result = error;
-            console.log(error);
-        }else{
-            console.log('Message sent: ' + info.response);
-            result = info.response;
-        }
-    });
 }
 
-module.exports = Mail;
+module.exports = new Mail;
