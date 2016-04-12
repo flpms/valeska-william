78ea736ed41b3811beb4460a74f2db7875756156
Filipe M. Silva <filipemelodasilva@gmail.com>
2016-04-12 13:37:20 -0300

    Update Mail.js

--- routes/modules/Mail.js
+++ routes/modules/Mail.js
@@ -23,11 +23,7 @@ var Mail = function() {
 
     self.sendEmail = function(content, callback) {
         var generator = xoauth2.createXOAuth2Generator({
-            user : "filipemelodasilva@gmail.com",
-            clientSecret : "Oh1hiIjclgZg2Ew8utttckIU",
-            clientId: "272997199071-sueg5qpj4i1v0mlep1br6usio5qs335j.apps.googleusercontent.com",
-            refreshToken : "1/itBk6FJoX_YTH-QRP8pocR6nEchGS3pAqz7wEphe86s",
-            accessToken : "ya29.ZwFCtzF3Ng92868NbE51QAa8mheek_SE9fXS0qlxfiHgatGTuxSajSwYETng0IsFL58yeQyg8dF0LQ"
+            
         });
 
         generator.on('token', function(token){
